7739b27b74c4556bf6d3e0c8ea437f7f999b6a1b
intisar11 <100384942+intisar11@users.noreply.github.com>
2022-04-16 12:49:41 -0700

    intial stage password manager app

---
+++ password-manager-start/main.py
@@ -0,0 +1,29 @@
+from tkinter import *
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20)
+
+photo = PhotoImage(file="logo.png")
+
+canvas = Canvas(width= 200, height= 200)
+
+canvas.create_image(100, 100, image= photo)
+canvas.pack()
+
+
+
+
+
+
+
+window.mainloop()
+
