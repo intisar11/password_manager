8f7da93e1700b143a864c0d2a8f4e567ae5c9967
intisar11 <100384942+intisar11@users.noreply.github.com>
2022-04-17 13:21:36 -0700

    second stage

--- password-manager-start/main.py
+++ password-manager-start/main.py
@@ -6,18 +6,55 @@ from tkinter import *
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
+
+
+def save():
+    website = entry_1.get()
+    email = entry_2.get()
+    passwords = entry_3.get()
+    with open("data.txt", "a") as data_file:
+        data_file.write(f"{website} | {email} | {passwords}")
+        entry_1.delete(0, END)
+        entry_2.delete(0, END)
+        entry_3.delete(0, END)
+
+
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
 window.title("Password Manager")
-window.config(padx=20, pady=20)
+window.config(padx=50, pady=50)
 
 photo = PhotoImage(file="logo.png")
 
 canvas = Canvas(width= 200, height= 200)
 
 canvas.create_image(100, 100, image= photo)
-canvas.pack()
+canvas.grid(column= 1, row=0)
+
+website = Label(text="Website")
+website.grid(column=0, row=1)
+entry_1 = Entry(width=35)
+entry_1.focus()
+entry_1.grid(column=1, row=1, columnspan=2)
+
+
+email_username = Label(text="Email/Username:")
+email_username.grid(column=0, row=2)
+entry_2 = Entry(width=35)
+entry_2.insert(0, "intisar.a@live.com")
+entry_2.grid(column=1, row=2, columnspan=2)
+
+password = Label(text="Password:")
+password.grid(column=0, row=3)
+entry_3 = Entry(width=21)
+entry_3.grid(column=1, row=3)
+
+generate_password = Button(text="Generate Password")
+generate_password.grid(column=2, row=3)
+
+add = Button(text="Add", width=36, command=save)
+add.grid(column=1, row=4, columnspan=2)
 
 
 
