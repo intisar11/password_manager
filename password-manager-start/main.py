75ed94a1d317136868fd3e99db25d7e970d69018
intisar11 <100384942+intisar11@users.noreply.github.com>
2022-04-19 14:45:41 -0700

    final stage.

--- password-manager-start/main.py
+++ password-manager-start/main.py
@@ -1,6 +1,35 @@
 from tkinter import *
+from tkinter import messagebox
+
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+#Password Generator Project
+import random
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+
+
+    password_list = [random.choice(letters) for _ in range(nr_letters)]
+    password_list1 = [random.choice(symbols) for _ in range(nr_symbols)]
+    password_list2 = [random.choice(numbers) for _ in range(nr_numbers)]
+
+    new_password_list = password_list2 + password_list1 + password_list
+
+    random.shuffle(new_password_list)
+
+    password = "".join(new_password_list)
+    entry_3.insert(0, password)
+
+
+
+
 
 
 
@@ -12,11 +41,22 @@ def save():
     website = entry_1.get()
     email = entry_2.get()
     passwords = entry_3.get()
-    with open("data.txt", "a") as data_file:
-        data_file.write(f"{website} | {email} | {passwords}\n")
-        entry_1.delete(0, END)
-        entry_2.delete(0, END)
-        entry_3.delete(0, END)
+
+
+
+
+    if len(website) == 0 or len(email) == 0 or len(passwords) == 0:
+        messagebox.showinfo(title="Error", message="You can not leave any box empty")
+
+    else:
+        is_ok = messagebox.askokcancel(title=website,
+                                   message=f"These are the details entered:\n Email:{email}\nPasswords:{passwords}\is it ok?")
+        if is_ok:
+            with open("data.txt", "a") as data_file:
+                data_file.write(f"{website} | {email} | {passwords}\n")
+                entry_1.delete(0, END)
+                entry_2.delete(0, END)
+                entry_3.delete(0, END)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -50,7 +90,7 @@ password.grid(column=0, row=3)
 entry_3 = Entry(width=21)
 entry_3.grid(column=1, row=3)
 
-generate_password = Button(text="Generate Password")
+generate_password = Button(text="Generate Password", command=generate_password)
 generate_password.grid(column=2, row=3)
 
 add = Button(text="Add", width=36, command=save)
